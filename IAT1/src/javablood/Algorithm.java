4ae027f8327f3228dbaefe13a1e38b6de2c0741b
Max Zorzetti <maxzorzetti@live.com>
2017-04-08 00:40:38 -0300

    Separated Point class from Algorithm

--- IAT1/src/javablood/Algorithm.java
+++ IAT1/src/javablood/Algorithm.java
@@ -2,12 +2,6 @@ package javablood;
 
 public interface Algorithm {
 	
-	public class Point { 
-		int x; 
-		int y; 
-		Point (int x, int y) { this.x = x; this.y = y; }
-	};
-	
 	public Point calculateNextMove(Point start, Point end, Environment environment);
 	
 }
