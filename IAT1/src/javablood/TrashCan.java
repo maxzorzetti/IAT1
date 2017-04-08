435ca7e3f9069613701d7ed158f9b5b9a8bda219
MatheusVaccaro <matheusvaccaro@icloud.com>
2017-04-08 01:23:42 -0300

    TrashCan class added

---
+++ IAT1/src/javablood/TrashCan.java
@@ -0,0 +1,42 @@
+package javablood;
+
+public class TrashCan {
+	private Point coordinates;
+	private int capacity;
+	private int content;
+	
+	public TrashCan(Point coordinates, int capacity){
+		this.coordinates = coordinates;
+		this.capacity = capacity;
+	}
+	
+	public TrashCan(int x, int y, int capacity){
+		this.coordinates = new Point(x, y);
+		this.capacity = capacity;
+	}
+	
+	public Point getCoordinates(){
+		return coordinates;
+	}
+	
+	public int getCapacity(){
+		return capacity;
+	}
+	
+	public void cleanTrashCan(){
+		content = 0;
+	}
+	
+	public boolean addTrash(){
+		if(content < capacity){
+			content++;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isFull(){
+		if(content >= capacity) return true;
+		return false;
+	}
+}
